4b753ec88e7c52e03e82e23ac4bb8e3291289607
isaiahrivera21 <isaiahcooperdump@gmail.com>
2024-02-19 21:31:50 -0500

    Made template for the Special Spaces

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,6 +1,15 @@
 package game;
 
+import static java.lang.Math.abs;
+
 public class Board {
     // insert stuff here
+
+
+
+    private boolean passedSpace(int prevSpaceNum, int currSpaceNum, int specialSpaceNum)
+    {
+        return abs(currSpaceNum - prevSpaceNum) > specialSpaceNum;
+    }
 
 }
--- src/game/Special.java
+++ src/game/Special.java
@@ -1,5 +1,8 @@
 package game;
 
+
+import java.util.Scanner;
+
 class Special extends Space
 {
     public String spaceName;
@@ -7,30 +10,44 @@ class Special extends Space
 
     private void startSpace()
     {
-
+            // put 300 dollars in the users account for landing directly on jail
+            System.out.println("You've received 300 dollars for passing START");
     }
     private  void debtSpace()
     {
+        //pull from the revoir in the database
+        // change that to zero
 
+        System.out.println("BANGPOTT!!! You received all the debt money");
     }
 
     private void vactionSpace()
     {
-
+        System.out.println("Welcome to paradise");
+        // literally does nothing
     }
 
     private void reverseSpace()
     {
-
+        System.out.println("Turn Around");
+        // modify direction in the player database
     }
     private  void evictionSpace()
     {
-
+        System.out.println("Sorry Bud you loosing a random house");
+        // modify number of hotels. Decrement it by 1.
     }
 
     private void jailSpace()
     {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Would you like to pay bail[Yes/No]: ");
+        String s = scanner.nextLine();
 
+        if (s.equals("Yes"))
+        {
+            // take 50 dollars from the user
+        }
     }
 
     @Override
@@ -64,7 +81,6 @@ class Special extends Space
                 jailSpace();
                 break;
         }
-
     }
 }
 
