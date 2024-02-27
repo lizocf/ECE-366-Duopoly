b56cd3821341f1c224a3cbe28afed77737693aad
lizocf <lizelle.ocfemia@cooper.edu>
2024-02-26 20:41:49 -0500

    add three rounds and Dockerfile

--- Dupoly-Maven/database/sample_data.sql
+++ Dupoly-Maven/database/sample_data.sql
@@ -67,9 +67,10 @@ UPDATE player_in_game
 SET cash = 0,
 
 -- mudkip loses
-    afk = TRUE 
-    -- is this a good way to show that someone lost? alternatively, we can
-    -- just remove this player from this table when they quit
+    dead = TRUE
+    -- now in spectator mode
+    -- should be used to skip turn
+    -- remove this player from this table when they quit/exit the game
 WHERE game_id = 1 AND user_id = 2;
 
 -- esaya's turn
@@ -82,7 +83,7 @@ SET current_direction = 'LEFT',
     current_position = 52   -- going counter clockwise now
 WHERE game_id = 1 AND user_id = 3;
 
--- suprise card pulled: claim all of Cali
+-- suprise card pulled: Cali Crib: gain all of Cali
 
 INSERT INTO owned_property(game_id, user_id, set_name, property_name)
 VALUES (1,3,'California', 'Los Angeles'),
@@ -109,12 +110,150 @@ UPDATE owned_property
 SET num_hotels = 1
 WHERE game_id = 1 AND user_id = 3 AND property_name = 'Los Angeles';
 
--- airport
+-- new round (2)
+UPDATE game_meta
+SET num_turns = 2
+WHERE game_id = 1;
 
--- sells property
+-- spice's turn
+UPDATE game_meta 
+SET which_player_turn = 1 
+WHERE game_id = 1; 
+
+UPDATE player_in_game
+SET current_position = 15 -- rolled a 4, land on airport
+WHERE game_id = 1 AND user_id = 1; -- game_id part overkill bc users arent allowed in > 1 game, safer ig
+
+-- buy airport
+UPDATE player_in_game
+SET cash = 1840 -- airport = $100
+WHERE game_id = 1 AND user_id = 1;
+
+INSERT INTO owned_property(game_id, user_id, set_name, property_name)
+VALUES (1,1,'Airports', 'DTW');
+
+-- esaya's turn
+UPDATE game_meta 
+SET which_player_turn = 3 
+WHERE game_id = 1; 
+
+UPDATE player_in_game
+SET current_position = 40 -- rolled 12
+WHERE game_id = 1 AND user_id = 3;
+
+-- eviction space: lose a hotel
+UPDATE owned_property
+SET num_hotels = 0
+WHERE game_id = 1 AND user_id = 3 AND property_name = 'Los Angeles';
+
+-- esaya rolled a double! gain extra turn :D
+UPDATE player_in_game
+SET current_position = 30 -- rolled 10, reverse space
+WHERE game_id = 1 AND user_id = 3;
+
+-- lands on reverse space
+UPDATE player_in_game
+SET current_direction = 'RIGHT'
+WHERE game_id = 1 AND user_id = 3;
+
+-- new round (3)
+UPDATE game_meta
+SET num_turns = 3
+WHERE game_id = 1;
+
+-- spice's turn
+UPDATE game_meta 
+SET which_player_turn = 1 
+WHERE game_id = 1; 
+
+UPDATE player_in_game
+SET current_position = 18 -- rolled 3, landed on skull
+WHERE game_id = 1 AND user_id = 1;
+
+-- skull card: JAIL :(
+UPDATE player_in_game
+SET current_position = 50,
+    jail = TRUE, -- added jail bool, be used to skip their turn
+WHERE game_id = 1 AND user_id = 1;
+
+-- esaya's turn
+UPDATE game_meta 
+SET which_player_turn = 3 
+WHERE game_id = 1; 
+
+UPDATE player_in_game
+SET current_position = 32 -- going clockwise! rolled 2
+WHERE game_id = 1 AND user_id = 3;
+
+-- surprise card: debt collector
+UPDATE game_meta
+SET debt_pot = 0
+WHERE game_id = 1;
+
+UPDATE player_in_game
+SET cash = 1870 -- +$50 from debt pot
+WHERE game_id = 1 AND user_id = 3;
+
+-- new round (4)
+UPDATE game_meta
+SET num_turns = 4
+WHERE game_id = 1;
+
+-- spice's turn, but must roll double to get out of jail.
+UPDATE game_meta
+SET which_player_turn = 1
+WHERE game_id = 1;
+
+-- pretend spice rolled a double!
+UPDATE player_in_game
+SET jail = FALSE, -- added jail bool, be used to skip their turn
+    current_position = 52 -- going clockwise! rolled 2
+WHERE game_id = 1 AND user_id = 1;
+
+-- surprise card landed: go to LA!
+UPDATE player_in_game
+SET current_position = 31 -- going clockwise! rolled 2
+WHERE game_id = 1 AND user_id = 1;
+
+
+-- esaya owns this property, spice must pay him $500
+UPDATE player_in_game
+SET cash = 2370 -- +$500 from spice
+WHERE game_id = 1 AND user_id = 3;
+
+UPDATE player_in_game
+SET cash = 1340 --  -$500 
+WHERE game_id = 1 AND user_id = 1;
+
+-- mudkip exits game
+DELETE FROM player_in_game WHERE game_id = 1 and user_id = 2;
+
+UPDATE game_meta
+SET num_players = 2
+WHERE game_id = 1;
+
+-- mudkip gains 10 dp and num_losses = 1!
+UPDATE accounts
+SET num_losses = 1,
+    duo_points = 10
+WHERE user_id = 2;
+
+-- esaya's turn
+UPDATE game_meta 
+SET which_player_turn = 3 
+WHERE game_id = 1; 
+
+UPDATE player_in_game
+SET current_position = 44 -- rolled 12 again! on skull L
+WHERE game_id = 1 AND user_id = 3;
+
+-- skull card: WEST COAST WORST COAST
+DELETE FROM owned_property
+WHERE game_id = 1 and user_id = 3 and set_name = 'California';
+
 
 -- end game
-DELETE FROM "game_meta" WHERE "game_id" = 1;
+-- DELETE FROM "game_meta" WHERE "game_id" = 1;
 -- delete on cascade !! (player_in_game rows get deleted too)
 
-ALTER SEQUENCE game_meta_game_id_seq RESTART WITH 1;
+-- ALTER SEQUENCE game_meta_game_id_seq RESTART WITH 1;
