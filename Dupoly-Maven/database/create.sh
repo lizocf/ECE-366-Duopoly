57851fba0e041bc5dda6d3fc50485f40830453f9
lizocf <lizelle.ocfemia@cooper.edu>
2024-03-01 21:21:39 -0500

    adjust 'JDBCExecutor.java' for docker

--- Dupoly-Maven/database/create.sh
+++ Dupoly-Maven/database/create.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-database=test
+database=duopoly
 
 # # drop all current tables
 # psql -h localhost -U postgres -d $database -f database/drop_all.sql
 
 
 # # create all tables
-# psql -h localhost -U postgres -d $database -f database/all_property.sql
-# psql -h localhost -U postgres -d $database -f database/all_sets.sql
-# psql -h localhost -U postgres -d $database -f database/elo.sql
+psql -h localhost -U postgres -d $database -f database/all_property.sql
+psql -h localhost -U postgres -d $database -f database/all_sets.sql
+psql -h localhost -U postgres -d $database -f database/elo.sql
 
 psql -h localhost -U postgres -d $database -f database/game_meta.sql
 psql -h localhost -U postgres -d $database -f database/owned_property.sql
--- Dupoly-Maven/src/main/java/jdbc/JDBCExecutor.java
+++ Dupoly-Maven/src/main/java/jdbc/JDBCExecutor.java
@@ -9,13 +9,16 @@ public class JDBCExecutor
 {
 
     public static void main(String... args) {
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
-                "duopoly", "postgres", "password");
+        // DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+                // "duopoly", "postgres", "password");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("db",
+                "duopoly", "postgres", "password"); 
+
 
         try {
             Connection connection = dcm.getConnection();
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM player");
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM player_in_game");
             while(resultSet.next()){
                 System.out.println(resultSet.getInt(1));
             }
