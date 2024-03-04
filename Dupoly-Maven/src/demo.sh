b63dd054e1059b165cb502cc8836d9622167646b
lizocf <lizelle.ocfemia@cooper.edu>
2024-03-04 13:39:46 -0500

    add demo

--- Dupoly-Maven/src/demo.sh
+++ Dupoly-Maven/src/demo.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
 
+# create Clarence account
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"user_name": "Clarence"}' \
   "http://localhost:8080/createNewAccount"
 
+# create Jeffery account
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"user_name": "Jeffery"}' \
   "http://localhost:8080/createNewAccount"
 
+# create TimmyJones
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"user_name": "TimmyJones"}' \
   "http://localhost:8080/createNewAccount"
 
+# create new game with game code AS123
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"game_code": "AS123"}' \
   "http://localhost:8080/createNewGame"
 
+# add player with user_id 1 into PlayerInGame
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+    "user_id": "1",
+    "game_id": "1"
+  }' \
+  "http://localhost:8080/createPlayerInGame"
+
+# add player with user_id 2 into PlayerInGame
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{
     "user_id": "2",
+    "game_id": "1"
+  }' \
+  "http://localhost:8080/createPlayerInGame"
+
+# add player with user_id 3 into PlayerInGame
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+    "user_id": "3",
     "game_id": "1"
   }' \
   "http://localhost:8080/createPlayerInGame"
