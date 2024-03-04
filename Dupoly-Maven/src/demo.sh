936e5bd6149c63eb5d32793963f7e853180d1af8
Nicholas Storniolo <nicholas.storniolo@cooper.edu>
2024-03-04 15:06:37 -0500

    prepping demo.sh, error with updateDir

--- Dupoly-Maven/src/demo.sh
+++ Dupoly-Maven/src/demo.sh
@@ -51,20 +51,82 @@ curl --header "Content-Type: application/json" \
   }' \
   "http://localhost:8080/createPlayerInGame"
 
+# user_id 2 will go first
+# user_id 2 chooses their starting direction
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+    "game_id": "1"
+    "user_id": "2"
+    "direction": "right"
+    }' \
+    "http://localhost:8080/updateDir"
+
+# user_if 2 rolls and moves 2
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+    "game_id": "1"
+    "user_id": "2"
+    "move_to": "2"
+  }'
+# user_id 2 chooses to purchase space 2
 curl --header "Content-Type: application/json" \
   --request GET \
   --data '{
-    "space" : "1",
+    "space" : "2",
     "user_id": "2",
     "game_id": "1"
   }' \
   "http://localhost:8080/createOwnedProperty"
 
+# user_id 2 pays for space 2
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{
     "user_id": "2",
     "game_id": "1",
-    "profit" : "-1500"
+    "profit" : "-500"
   }' \
   "http://localhost:8080/updateCash"
+
+# user_id 1 chooses their starting direction
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+    "game_id": "1"
+    "user_id": "1"
+    "direction": "left"
+    }' \
+    "http://localhost:8080/updateDir"
+
+# user_id 1 moves to space 6
+curl --header "Content-Type: application/json:" \
+  --request POST \
+  --data '{
+    "user_id": "1",
+    "game_id": "1",
+    "move_to": "54"
+    }' \
+    "http://localhost:8080/updatePos"
+
+# user_id 1 chooses to purchase space 54
+curl --header "Content-Type: application/json" \
+  --request GET \
+  --data '{
+    "space" : "54",
+    "user_id": "1",
+    "game_id": "1"
+  }' \
+  "http://localhost:8080/createOwnedProperty"
+
+# user_id 1 pays for space 54
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+    "user_id": "1",
+    "game_id": "1",
+    "profit" : "-500"
+  }' \
+  "http://localhost:8080/updateCash"
+
