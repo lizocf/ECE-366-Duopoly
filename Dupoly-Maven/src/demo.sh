7dc9d78b82b11ba34f7c8d91c058ce76000b8cc9
isaiahrivera21 <isaiahcooperdump@gmail.com>
2024-03-04 11:44:20 -0500

    shell script to run demo for presentation working

--- Dupoly-Maven/src/demo.sh
+++ Dupoly-Maven/src/demo.sh
@@ -1,40 +1,47 @@
+#!/bin/bash
+
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"user_name": "Clarence"}' \
-  http://localhost:8080/createNewAccount
+  "http://localhost:8080/createNewAccount"
+
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"user_name": "Jeffery"}' \ 
-  http://localhost:8080/createNewAccount
+  --data '{"user_name": "Jeffery"}' \
+  "http://localhost:8080/createNewAccount"
+
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"user_name": "TimmyJones"}' \
-  http://localhost:8080/createNewAccount
+  "http://localhost:8080/createNewAccount"
+
 curl --header "Content-Type: application/json" \
   --request POST \
-  --data '{"game_code": "AS123"}' \     
-  http://localhost:8080/createNewGame
+  --data '{"game_code": "AS123"}' \
+  "http://localhost:8080/createNewGame"
+
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{
     "user_id": "2",
     "game_id": "1"
-          }' \
-  http://localhost:8080/createPlayerInGame
+  }' \
+  "http://localhost:8080/createPlayerInGame"
+
 curl --header "Content-Type: application/json" \
   --request GET \
   --data '{
     "space" : "1",
     "user_id": "2",
     "game_id": "1"
-          }' \
-  http://localhost:8080/createOwnedProperty
+  }' \
+  "http://localhost:8080/createOwnedProperty"
+
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{
     "user_id": "2",
     "game_id": "1",
     "profit" : "-1500"
-          }' \
-  http://localhost:8080/updateCash
-  
+  }' \
+  "http://localhost:8080/updateCash"
